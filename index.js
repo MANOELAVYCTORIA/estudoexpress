757fdf987599de86cfe120f4c845f91abbd02b15
manoelavyctoria <manuelavyctoria35@gmail.com>
2021-03-31 11:03:41 -0300

    Adicionando GET e POST

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+const { request, response } = require('express');
 const express = require('express');
-
 const app = express();
 
-console.log(app);
+//console.log(app);
+
+app.get('/projetos', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+    ])
+});
+
+app.post('/projetos', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+    ])
+})
+
+app.listen(8080);
+  
